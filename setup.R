d6e4797fcc6f822f83a78745efc5a99cb81f48a7
Anil Tellbuescher <anil@tellbuescher.online>
2025-01-13 21:28:00 +0100

    add targets package and explanation for why to use the here package

--- setup.R
+++ setup.R
@@ -1,5 +1,10 @@
-# Load packages
+# LOAD PACKAGES
+
+# The use of 'here' is encouraged because it eliminates issues with cross-
+# platform portability of the R code due to file path issues.
 library(here)
+
 library(readxl)
 library(tidyverse)
 library(knitr)
+library(targets)
