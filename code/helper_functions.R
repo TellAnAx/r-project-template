0969c3627c024fc73edd7badd1db93e44b2c413d
Anil Tellbuescher <anil@tellbuescher.online>
2025-01-13 21:32:03 +0100

    add descriptions

--- code/helper_functions.R
+++ code/helper_functions.R
@@ -1,1 +1,4 @@
-# Functions
+# HELPER FUNCTIONS
+#
+# Helper functions are small functions for small repetitive tasks such as 
+# table styling or specific settings for plots.
--- setup.R
+++ setup.R
@@ -1,4 +1,9 @@
 # LOAD PACKAGES
+#
+# The setup script is supposed to provide an easy to find collection of all
+# packages that are required to run the code in the entire repository. This
+# way it is not necessary to look for specific files that provide specific 
+# packages and it is also not necessary to load a package all the time.
 
 # The use of 'here' is encouraged because it eliminates issues with cross-
 # platform portability of the R code due to file path issues.
@@ -7,4 +12,7 @@ library(here)
 library(readxl)
 library(tidyverse)
 library(knitr)
+
+# The 'targets' package is supposed to make the data analysis pipeline 
+# easier understandable and errors within the pipeline easier to find.
 library(targets)
