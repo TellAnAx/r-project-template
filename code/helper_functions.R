ec6118cac84f5b36b6d880e8d9bb9d957150a4ef
Anil Tellbuescher <anil@tellbuescher.online>
2025-01-14 18:04:45 +0100

    add shortcut functions for data paths

--- code/helper_functions.R
+++ code/helper_functions.R
@@ -2,3 +2,45 @@
 #
 # Helper functions are small functions for small repetitive tasks such as 
 # table styling or specific settings for plots.
+
+
+here_data <- function(filename,
+                      directory = "data"
+) {
+  here::here(directory, filename)
+}
+
+
+
+here_code <- function(filename,
+                      directory = "code"
+) {
+  here::here(directory, filename)
+}
+
+
+
+here_interm <- function(filename,
+                        directory = "output",
+                        subdirectory = "interm"
+) {
+  here::here(directory, subdirectory, filename)
+}
+
+
+
+here_plots <- function(filename,
+                       directory = "output",
+                       subdirectory = "plots"
+) {
+  here::here(directory, subdirectory, filename)
+}
+
+
+
+here_tables <- function(filename,
+                        directory = "output",
+                        subdirectory = "tables"
+) {
+  here::here(directory, subdirectory, filename)
+}
